349899d660d0afb51e0aa140fce0ee2465e0d1b2
Nitesh Kumar <nitesh.kumar283@gmail.com>
2020-05-04 03:27:07 +0530

    added blueprint js lib

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
 import values from 'lodash/values';
 import Tree, { TreeNode } from "rc-tree";
-import "rc-tree/assets/index.css";
+import SplitPane, { Pane } from 'react-split-pane';
 
+import {
+  Button,
+  Classes,
+  Navbar,
+  NavbarDivider,
+  NavbarGroup,
+  Popover,
+  Menu,
+  MenuItem,
+  InputGroup
+} from '@blueprintjs/core';
+
+import "rc-tree/assets/index.css";
+import "normalize.css";
+import "@blueprintjs/core/lib/css/blueprint.css";
+import "@blueprintjs/icons/lib/css/blueprint-icons.css";
 
 const {dialog} = window.require('electron').remote
 const dirTree = window.require("directory-tree");
+
+const showMessageBox = window.require('electron').remote.dialog.showMessageBox;
+const app = window.require('electron').remote.app;
 
 class App extends Component {
 
@@ -17,37 +36,69 @@ class App extends Component {
     }
   }
 
-  onClickHandler = (event) => {
-    console.log(event.target.name);
-    this.openDialog(event.target.name);
+  onClickHandler = (btnName) => {
+    this.openDialog(btnName);
   }
 
-  openDialog = (targetName) => {
+  handleInputChange = (id, evt) => {
+    console.log(evt.target.value);
+    switch(id){
+      case 'pathFrom':
+        this.setState({
+          filePath1: evt.target.value
+        });
+        break;
+      case 'pathTo':
+        this.setState({
+          filePath2: evt.target.value
+        });
+        break;
+      default:
+    }
+  }
+
+  handleEnterPress = (id, event) => {
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      event.stopPropagation();
+      switch(id){
+        case 'pathFrom':
+          this.renderDirectories(this.state.filePath1);
+          break;
+        case 'pathTo':
+          this.renderDirectories(this.state.filePath2);
+          break;
+        default:
+      }
+    }
+  }
+
+  openDialog = (btnName) => {
     dialog.showOpenDialog({
-      properties: ['openFile', 'openDirectory']
+      properties: ['openDirectory']
     }).then(result => {
-        console.log(result.filePaths);
-        if(result.filePaths){
-            switch(targetName){
-                case 'SelectFIleBtn1':
-                  this.setState({
-                    filePath1 : `${result.filePaths}`
-                  },() => {
-                    console.log(dirTree(this.state.filePath1, {attributes:['ctime', 'mtime']}))
-                    //this.renderDirectories(this.state.filePath1);
-                  });
-                    break;
-                case 'SelectFIleBtn2':
-                  this.setState({
-                    filePath2 : `${result.filePaths}`
-                  },() => {
-                    console.log(dirTree(this.state.filePath2, {attributes:['ctime', 'mtime']}))
-                    //this.renderDirectories(this.state.filePath2);
-                  });
-                    break;
-                default:
-            }
+      console.log(result.filePaths);
+      if (result.filePaths) {
+        switch (btnName) {
+          case 'SelectFileBtn1':
+            this.setState({
+              filePath1: `${result.filePaths}`
+            }, () => {
+              console.log(dirTree(this.state.filePath1, { attributes: ['ctime', 'mtime'] }))
+              //this.renderDirectories(this.state.filePath1);
+            });
+            break;
+          case 'SelectFileBtn2':
+            this.setState({
+              filePath2: `${result.filePaths}`
+            }, () => {
+              console.log(dirTree(this.state.filePath2, { attributes: ['ctime', 'mtime'] }))
+              //this.renderDirectories(this.state.filePath1);
+            });
+            break;
+          default:
         }
+      }
     })
   }
 
@@ -75,7 +126,7 @@ class App extends Component {
       return (
         <TreeNode
           icon = ""
-          key = {index}
+          key = {`${each.path}-${index}`}
           title = {each.name}
           value = {each.path}>
             {this.buildTreeNodes(each.children)}
@@ -87,24 +138,87 @@ class App extends Component {
   render(){
     return (
       <>
-        <div style={{height:"100%", width:"50%", position:"fixed", zIndex:1, top:0, left:0, borderRight:"1px solid red", marginLeft:"5px"}}>
-          <label>Path 1: </label>
-          <input type="text"
-            style={{width:"75%", borderColor:"black"}}
+        <Navbar>
+          <NavbarGroup>
+            <Button className={Classes.MINIMAL} icon="folder-open" />
+            <Popover content={
+              <Menu>
+                <MenuItem
+                  text="File 1"
+                  onClick={() => showMessageBox({
+                    type: 'info',
+                    message: 'You activated action: "file1"',
+                    buttons: ['Close'],
+                  })}
+                />
+              </Menu>}
+            >
+              <Button className={Classes.MINIMAL} icon="chevron-down" />
+            </Popover>
+            <Button
+              className={Classes.MINIMAL}
+              icon="small-cross"
+              onClick={() => app.quit()}
+            />
+            <NavbarDivider />
+            <Button
+              className={Classes.MINIMAL}
+              icon="properties"
+              onClick={() => showMessageBox({
+                type: 'info',
+                message: 'You activated action: "logo"',
+                buttons: ['Close'],
+              })}
+            />
+          </NavbarGroup>
+        </Navbar>
+      <SplitPane split="vertical" defaultSize={"50%"} primary="second">
+        <Pane initialSize="50%" minSize="25%" maxSize="50%">
+          <InputGroup
+            type="text"
+            disabled={false}
+            large={true}
+            leftIcon={this.state.filePath1.length > 0 ? "folder-open" : "folder-close"}
+            onChange={e => this.handleInputChange('pathFrom',e)}
+            onKeyDown={e => this.handleEnterPress('pathFrom', e)}
+            placeholder="Path from copy files"
+            fill={true}
             value={this.state.filePath1}
-          />
-          <button name="SelectFIleBtn1" style={{borderColor:"black", marginLeft:"5px"}} onClick={this.onClickHandler}>Select Files</button>
+            style={{width:"80%",marginLeft:"5px"}}
+            rightElement={
+            <Button 
+              icon="select"
+              className={Classes.BUTTON}
+              onClick={() => this.onClickHandler('SelectFileBtn1')}
+              style={{marginRight:"25px"}}
+              >Select</Button>
+            }
+            />
           {this.renderDirectories(this.state.filePath1)}
-        </div>
-        <div style={{height:"100%", width:"50%", position:"fixed", zIndex:1, top:0, right:0, borderRight:"1px solid red"}}>
-          <label style={{marginLeft:"15px"}}>Path 2: </label>
-          <input type="text"
-            style={{width:"75%", borderColor:"black"}}
+        </Pane>
+        <Pane initialSize="50%" minSize="25%" maxSize="50%">
+        <InputGroup
+            type="text"
+            disabled={false}
+            large={true}
+            leftIcon={this.state.filePath2.length > 0 ? "folder-open" : "folder-close"}
+            onChange={e => this.handleInputChange('pathTo',e)}
+            placeholder="Path to copy files"
+            fill={true}
             value={this.state.filePath2}
-          />
-          <button name="SelectFIleBtn2" style={{borderColor:"black", marginLeft:"5px"}} onClick={this.onClickHandler}>Select Files</button>
+            style={{width:"80%",marginLeft:"5px"}}
+            rightElement={
+            <Button 
+              icon="select"
+              className={Classes.BUTTON}
+              onClick={() => this.onClickHandler('SelectFileBtn2')}
+              style={{marginRight:"25px"}}
+              >Select</Button>
+            }
+            />
           {this.renderDirectories(this.state.filePath2)}
-        </div>
+        </Pane>
+        </SplitPane>
       </>
     );
   }
